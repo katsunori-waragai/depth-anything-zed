951b622ce38347b7b5a66518c64fd25b55ed4632
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-07-02 13:36:12 +0900

    simplified Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -32,46 +32,6 @@ ENV ZED_SDK_INSTALLER=ZED_SDK_Tegra_L4T35.3_v4.1.0.zstd.run
 RUN wget --quiet -O ${ZED_SDK_INSTALLER} https://download.stereolabs.com/zedsdk/4.1/l4t35.2/jetsons
 RUN chmod +x ${ZED_SDK_INSTALLER} && ./${ZED_SDK_INSTALLER} -- silent
 
-WORKDIR /root/Grounded-Segment-Anything
-RUN python3 -m pip install --no-cache-dir -e segment_anything
-
-RUN python3 -m pip install --no-cache-dir wheel
-RUN python3 -m pip install --no-cache-dir --no-build-isolation -e GroundingDINO
-
-WORKDIR /root
-RUN python3 -m pip uninstall --yes opencv-python-headless opencv-contrib-python
-RUN python3 -m pip install --no-cache-dir opencv-python==3.4.18.65 \
-    pycocotools==2.0.6 matplotlib==3.5.3 \
-    onnxruntime==1.14.1 onnx==1.13.1 scipy mediapipe scikit-image
-RUN python3 -m pip install gdown
-# for depth anything
-RUN python3 -m pip install -U pip
-RUN python3 -m pip install loguru tqdm thop ninja tabulate
-RUN python3 -m pip install pycocotools
-RUN python3 -m pip install -U jetson-stats 
-RUN python3 -m pip install huggingface_hub onnx
-
-
-# download pre-trained files
-WORKDIR /root/Grounded-Segment-Anything
-RUN wget --quiet https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
-# following models are optional
-RUN wget --quiet https://dl.fbaipublicfiles.com/segment_anything/sam_vit_l_0b3195.pth
-# RUN wget --quiet https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth
-# RUN wget --quiet https://huggingface.co/lkeab/hq-sam/resolve/main/sam_hq_vit_tiny.pth
-
-RUN wget --quiet https://github.com/IDEA-Research/GroundingDINO/releases/download/v0.1.0-alpha/groundingdino_swint_ogc.pth
-
-# following models are optional for HQ models
-# RUN gdown --fuzzy https://drive.google.com/file/d/11yExZLOve38kRZPfRx_MRxfIAKmfMY47/view?usp=sharing
-# RUN gdown --fuzzy https://drive.google.com/file/d/1qobFYrI4eyIANfBSmYcGuWRaSIXfMOQ8/view?usp=sharing
-# RUN gdown --fuzzy https://drive.google.com/file/d/1Uk17tDKX1YAKas5knI4y9ZJCo0lRVL0G/view?usp=sharing
-
-COPY *.sh *.py setup.cfg /root/Grounded-Segment-Anything/
-RUN mkdir -p zedhelper/
-RUN mkdir -p tutorial_script/
-COPY zedhelper/* /root/Grounded-Segment-Anything/zedhelper/
-COPY tutorial_script/*  /root/Grounded-Segment-Anything/tutorial_script/
 
 # for depth anything
 RUN cd /root && git clone https://github.com/IRCVLab/Depth-Anything-for-Jetson-Orin
---
+++ zed_cam.py
@@ -0,0 +1,163 @@
+"""
+depth画像を見るサンプルスクリプト
+
+"""
+
+import pyzed.sl as sl
+import argparse
+
+import cv2
+import numpy as np
+
+import skimage
+import matplotlib.pyplot as plt
+
+import zedhelper.handmark
+import zedhelper.util
+from zedhelper import predefined
+
+import inspect
+
+MAX_ABS_DEPTH, MIN_ABS_DEPTH = 0.0, 2.0  # [m]
+
+def parse_args(init):
+    if len(opt.input_svo_file) > 0 and opt.input_svo_file.endswith(".svo"):
+        init.set_from_svo_file(opt.input_svo_file)
+        print("[Sample] Using SVO File input: {0}".format(opt.input_svo_file))
+    elif len(opt.ip_address) > 0:
+        ip_str = opt.ip_address
+        if (
+            ip_str.replace(":", "").replace(".", "").isdigit()
+            and len(ip_str.split(".")) == 4
+            and len(ip_str.split(":")) == 2
+        ):
+            init.set_from_stream(ip_str.split(":")[0], int(ip_str.split(":")[1]))
+            print("[Sample] Using Stream input, IP : ", ip_str)
+        elif ip_str.replace(":", "").replace(".", "").isdigit() and len(ip_str.split(".")) == 4:
+            init.set_from_stream(ip_str)
+            print("[Sample] Using Stream input, IP : ", ip_str)
+        else:
+            print("Unvalid IP format. Using live stream")
+    if "HD2K" in opt.resolution:
+        init.camera_resolution = sl.RESOLUTION.HD2K
+        print("[Sample] Using Camera in resolution HD2K")
+    elif "HD1200" in opt.resolution:
+        init.camera_resolution = sl.RESOLUTION.HD1200
+        print("[Sample] Using Camera in resolution HD1200")
+    elif "HD1080" in opt.resolution:
+        init.camera_resolution = sl.RESOLUTION.HD1080
+        print("[Sample] Using Camera in resolution HD1080")
+    elif "HD720" in opt.resolution:
+        init.camera_resolution = sl.RESOLUTION.HD720
+        print("[Sample] Using Camera in resolution HD720")
+    elif "SVGA" in opt.resolution:
+        init.camera_resolution = sl.RESOLUTION.SVGA
+        print("[Sample] Using Camera in resolution SVGA")
+    elif "VGA" in opt.resolution:
+        init.camera_resolution = sl.RESOLUTION.VGA
+        print("[Sample] Using Camera in resolution VGA")
+    elif len(opt.resolution) > 0:
+        print("[Sample] No valid resolution entered. Using default")
+    else:
+        print("[Sample] Using default resolution")
+
+
+def resize_image(image: np.ndarray, rate: float) -> np.ndarray:
+    H, W = image.shape[:2]
+    return cv2.resize(image, (int(W * rate), int(H * rate)))
+
+
+def as_matrix(chw_array):
+    H_, W_ = chw_array.shape[-2:]
+    return np.reshape(chw_array, (H_, W_))
+
+
+def main(opt):
+    prompt = "bottle . person . box"
+    prompt = "bottle"
+    watching_obj = "bottle"
+    assert prompt.find(watching_obj) > -1
+
+    zed = sl.Camera()
+
+    init_params = predefined.InitParameters()
+
+    parse_args(init_params)
+
+    init_params.depth_mode = sl.DEPTH_MODE.ULTRA
+
+    err = zed.open(init_params)
+    if err != sl.ERROR_CODE.SUCCESS:
+        print(err)
+        exit(1)
+
+    zedhelper.util.show_params(init_params)
+
+    depth_map = sl.Mat()
+    point_cloud = sl.Mat()
+
+    runtime_parameters = predefined.RuntimeParameters()
+    runtime_parameters.measure3D_reference_frame = sl.REFERENCE_FRAME.WORLD
+    # runtime_parameters.measure3D_reference_frame = sl.REFERENCE_FRAME.CAMERA
+    runtime_parameters.confidence_threshold = opt.confidence_threshold
+    print(f"### {runtime_parameters.confidence_threshold=}")
+    zedhelper.util.show_params(runtime_parameters)
+
+    condition_str = f"mode: {init_params.depth_mode} conf: {runtime_parameters.confidence_threshold}"
+
+    while True:
+        if zed.grab(runtime_parameters) == sl.ERROR_CODE.SUCCESS:
+            zed.retrieve_measure(depth_map, sl.MEASURE.DEPTH)  # Retrieve depth
+            depth_map_data = depth_map.get_data()
+
+            # 空間座標を得ることが必要。
+            zed.retrieve_measure(point_cloud, sl.MEASURE.XYZRGBA)
+            points = point_cloud.get_data()
+            print(f"{points.shape=}")
+
+            # 点群の色情報が有効な領域をvalid_points_maskとして取得する。
+            points_color = points[:, :, 3]
+            valid_points_mask = np.isfinite(points_color)
+            print(f"{valid_points_mask.shape=} {valid_points_mask.dtype=}")
+            # points[y, x]で、元画像上の点と対応がつくのかどうか？
+
+            depth_map_data_modified = depth_map_data.copy()
+            print(f"{depth_map_data_modified.shape=} {depth_map_data_modified.dtype=}")
+            depth_map_data_modified[np.logical_not(valid_points_mask)] = np.nan
+
+    depth_map.free(memory_type=sl.MEM.CPU)
+    point_cloud.free(memory_type=sl.MEM.CPU)
+    zed.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--input_svo_file",
+        type=str,
+        help="Path to an .svo file, if you want to replay it",
+        default="",
+    )
+    parser.add_argument(
+        "--ip_address",
+        type=str,
+        help="IP Adress, in format a.b.c.d:port or a.b.c.d, if you have a streaming setup",
+        default="",
+    )
+    parser.add_argument(
+        "--resolution",
+        type=str,
+        help="Resolution, can be either HD2K, HD1200, HD1080, HD720, SVGA or VGA",
+        default="",
+    )
+    parser.add_argument(
+        "--confidence_threshold",
+        type=float,
+        help="depth confidence_threshold(0 ~ 100)",
+        default=100,
+    )
+    opt = parser.parse_args()
+    if len(opt.input_svo_file) > 0 and len(opt.ip_address) > 0:
+        print("Specify only input_svo_file or ip_address, or none to use wired camera, not both. Exit program")
+        exit()
+    main(opt)
