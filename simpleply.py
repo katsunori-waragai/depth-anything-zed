dfafcea3ca584adc06587aa53091377551d6795f
katsunori-waragai <huyuhiko1128@gmail.com>
2024-07-05 13:50:36 +0900

    revised todo.md

--- simpleply.py
+++ simpleply.py
@@ -4,10 +4,14 @@ import numpy as np
 
 
 def write_point_cloud(p: Path, xyzs: np.ndarray, colors: np.ndarray):
+    """
+    PointCloud のデータをファイルに書き込む。
+    """
     candidate_points = xyzs.shape[0]
     h, w, c = colors.shape
     colors = colors.reshape((h * w, c))
     print(f"{xyzs.shape=} {colors.shape=}")
+    assert xyzs.shape[0] == colors.shape[0]
     indexes = [i for i in range(candidate_points) if np.isfinite(xyzs[i, :]).all()]
 
     with open(p, "wt") as f:
--- todo.md
+++ todo.md
@@ -1,4 +1,11 @@
 # todo
+- pointCloud への変換の確認方法
+  - 球が球として計測できるか。
+- スケーリングの妥当性を確認できているか？
+- カメラ解像度と推論のための解像度の違いの扱いが妥当になっているか？
+  - focal_length_x, focal_length_y の値との関連はどうか
+- depth_anything を使ってdepthの値の絶対値を気にしている例はどれくらいあるのか？
+- depth_anything での平面の平面性はどんなであるか
 - 数値としてのdepthを点群データに変換して妥当性を確認しやすくすること
 - 以下のissue を読むと点群データへの変換と可視化の例が記されている。
 https://github.com/LiheYoung/Depth-Anything/issues/36
