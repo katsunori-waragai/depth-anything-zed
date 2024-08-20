ad1ad6b6ae55db476e5c71b7881492328aca9f41
katsunori-waragai <huyuhiko1128@gmail.com>
2024-08-20 18:55:58 +0900

    add fixed_intercept.py

--- fixed_intercept.py
+++ fixed_intercept.py
@@ -27,38 +27,39 @@ class FixedInterceptRegressor(BaseEstimator, RegressorMixin):
         return self
 
 
+if __name__ == "__main__":
+    # Example data
+    X = np.array([0, 1, 2, 3, 4, 5]).reshape(-1, 1)
+    y = np.array([1, 2.2, 3.1, 3.9, 5.3, 6.1])
 
-# Example data
-X = np.array([0, 1, 2, 3, 4, 5]).reshape(-1, 1)
-y = np.array([1, 2.2, 3.1, 3.9, 5.3, 6.1])
+    # Set the fixed intercept value
+    fixed_intercept = 0.5
 
-# Set the fixed intercept value
-fixed_intercept = 0.5
+    # Create an instance of the custom regressor
+    fixed_intercept_regressor = FixedInterceptRegressor(intercept=fixed_intercept)
 
-# Create an instance of the custom regressor
-fixed_intercept_regressor = FixedInterceptRegressor(intercept=fixed_intercept)
+    # Use RANSAC with the custom regressor
+    ransac = RANSACRegressor(estimator=fixed_intercept_regressor, min_samples=2, residual_threshold=0.5, max_trials=1000)
+    ransac.fit(X, y)
 
-# Use RANSAC with the custom regressor
-ransac = RANSACRegressor(estimator=fixed_intercept_regressor, min_samples=2, residual_threshold=0.5, max_trials=1000)
-ransac.fit(X, y)
+    # Get the estimated slope
+    estimated_slope = ransac.estimator_.slope_
+    print(f"Estimated slope: {estimated_slope}")
+    print(f"Fixed intercept: {fixed_intercept}")
 
-# Get the estimated slope
-estimated_slope = ransac.estimator_.slope_
-print(f"Estimated slope: {estimated_slope}")
-print(f"Fixed intercept: {fixed_intercept}")
+    # Get the inliers mask
+    inlier_mask = ransac.inlier_mask_
 
-# Get the inliers mask
-inlier_mask = ransac.inlier_mask_
+    # Predict using the fitted RANSAC model
+    line_X = np.linspace(X.min(), X.max(), 100).reshape(-1, 1)
+    line_y_ransac = ransac.predict(line_X)
 
-# Predict using the fitted RANSAC model
-line_X = np.linspace(X.min(), X.max(), 100).reshape(-1, 1)
-line_y_ransac = ransac.predict(line_X)
-
-# Plot the results
-plt.scatter(X[inlier_mask], y[inlier_mask], color='yellowgreen', marker='.', label='Inliers')
-plt.scatter(X[~inlier_mask], y[~inlier_mask], color='gold', marker='.', label='Outliers')
-plt.plot(line_X, line_y_ransac, color='cornflowerblue', label='Fitted Line')
-plt.xlabel("X")
-plt.ylabel("y")
-plt.legend(loc='best')
-plt.show()
+    # Plot the results
+    plt.scatter(X[inlier_mask], y[inlier_mask], color='yellowgreen', marker='.', label='Inliers')
+    plt.scatter(X[~inlier_mask], y[~inlier_mask], color='gold', marker='.', label='Outliers')
+    plt.plot(line_X, line_y_ransac, color='cornflowerblue', label='Fitted Line')
+    plt.xlabel("X")
+    plt.ylabel("y")
+    plt.legend(loc='best')
+    # plt.show()
+    plt.savefig("fixed_intercept.png")
