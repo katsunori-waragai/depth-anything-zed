e25163bafb79534951c7afc8402dd566fee85fca
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-08-21 10:44:35 +0900

    use default residual_threshold

--- depth_sensing_inv.py
+++ depth_sensing_inv.py
@@ -23,6 +23,7 @@ xyz_data = point_cloud.get_data()
 -　
 """
 
+
 import pyzed.sl as sl
 import math
 import numpy as np
@@ -71,7 +72,7 @@ class DepthComplementor:
         の入出力とする。
     """
 
-    ransac = sklearn.linear_model.RANSACRegressor(estimator=FixedInterceptRegressor(), min_samples=2, residual_threshold=0.5, max_trials=1000)
+    ransac = sklearn.linear_model.RANSACRegressor(estimator=FixedInterceptRegressor(), min_samples=2, residual_threshold=None, max_trials=1000)
     EPS = 1e-6
     predictable = False  # 最初のフィッティングがされないうちは、predict()できない。
 
