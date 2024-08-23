d40078f44887ec9ad1dc1d248bf0406dd79fe28e
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-08-23 13:48:10 +0900

    renamed module

--- depth_sensing_inv.py
+++ depth_sensing_inv.py
@@ -35,7 +35,7 @@ import sklearn.linear_model
 import matplotlib.pylab as plt
 
 from depth2pointcloud import disparity_to_depth, depth_to_disparity
-from depth_anything_zed.lib_depth_engine import DepthEngine, depth_as_colorimage, finitemin, finitemax
+from dep_any_zed.lib_depth_engine import DepthEngine, depth_as_colorimage, finitemin, finitemax
 
 
 def isfinite_near_pixels(
