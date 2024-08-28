909c8042a8055bbb393d319d9c0eb1e51b231f0c
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-08-28 11:01:20 +0900

    depth2pointcolud.py as module

--- depanyzed/depth2pointcolud.py
+++ depanyzed/depth2pointcolud.py
@@ -61,8 +61,8 @@ k3=-0.0123602
 """
 
 from dataclasses import dataclass
+
 import numpy as np
-import cv2
 
 
 @dataclass
@@ -112,54 +112,3 @@ def depth_to_disparity(depth: np.ndarray, baseline=119.987, focal_length=532.41)
         cy = 362.4065  # [pixel]
     """
     return baseline * focal_length / depth
-
-
-if __name__ == "__main__":
-    import simpleply
-
-    depth_file = "data/zed_depth.npy"
-    depth = np.load(depth_file)
-
-    rgb_file = "data/left.png"
-    img = cv2.imread(rgb_file)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-    # [LEFT_CAM_2K]
-    # fx = 1064.82
-    # fy = 1065.07
-    # cx = 1099.05
-    # cy = 628.813
-
-    if img.shape[1] == 1280:
-        # [LEFT_CAM_HD]
-        fx = 532.41
-        fy = 532.535
-        cx = 636.025  # [pixel]
-        cy = 362.4065  # [pixel]
-    else:
-        print(f"need setting for {image.shape}")
-        exit
-
-    H, W = depth.shape[:2]
-
-    depth2point = Depth2Points(fx, fy, cx, cy)
-    points = depth2point.cloud_points(depth)
-
-    assert depth.shape[:2] == img.shape[:2]
-    point_img = np.reshape(img, (H * W, 3))
-    selected_points = points[np.isfinite(depth.flatten())]
-    selected_img = point_img[np.isfinite(depth.flatten())]
-    print(f"{points.shape=}")
-    plyname = "data/test.ply"
-
-    # 点群の座標の原点を移動して、meshlab での表示を楽にする。
-    mean_point = np.mean(selected_points, axis=0)
-
-    centered_points = selected_points.copy()
-    centered_points[:, 0] -= mean_point[0]
-    centered_points[:, 1] -= mean_point[1]
-    centered_points[:, 2] -= mean_point[2]
-    plyname2 = "data/test_centered.ply"
-
-    simpleply.write_point_cloud(plyname, selected_points, selected_img)
-    simpleply.write_point_cloud(plyname2, centered_points, selected_img)
