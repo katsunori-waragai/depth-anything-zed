48d4f468785c4f4dd78683e90ff4ef1189b57826
Katsunori Waragai <149886409+katsunori-waragai@users.noreply.github.com>
2024-08-28 14:02:40 +0900

    added Makfile (#77)
    what
    Makefile を追加した。
    make test
    make reformat
    を可能にした。

--- test/test_camerainfo.py
+++ test/test_camerainfo.py
@@ -3,8 +3,9 @@ import pyzed.sl as sl
 
 from depanyzed.camerainfo import get_fx_fy_cx_cy, get_baseline
 
+
 def test_get_baseline():
-   # Create a ZED camera object
+    # Create a ZED camera object
     zed = sl.Camera()
 
     # Set up initial parameters for the camera
@@ -21,9 +22,10 @@ def test_get_baseline():
     baseline = get_baseline(cam_info)
     assert 110 < baseline < 130
     zed.close()
- 
+
+
 def test_get_fx_fy_cx_cy():
-    
+
     # Create a ZED camera object
     zed = sl.Camera()
 
@@ -43,15 +45,15 @@ def test_get_fx_fy_cx_cy():
     left_cam_params = cam_info.camera_configuration.calibration_parameters.left_cam
 
     fx, fy, cx, cy = get_fx_fy_cx_cy(left_cam_params)
-        
+
     assert isinstance(fx, float), "fx は float 型であるべきです"
     assert isinstance(fy, float), "fy は float 型であるべきです"
     assert isinstance(cx, float), "cx は float 型であるべきです"
     assert isinstance(cy, float), "cy は float 型であるべきです"
-    
+
     assert fx > 0, "fx は 0 より大きいべきです"
     assert fy > 0, "fy は 0 より大きいべきです"
     assert cx > 0, "cx は 0 より大きいべきです"
     assert cy > 0, "cy は 0 より大きいべきです"
-    
+
     zed.close()
