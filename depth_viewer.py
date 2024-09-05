e86d03db10eb44b574fdd5e36be60fdf67f45efd
katsunori-waragai <huyuhiko1128@gmail.com>
2024-09-05 16:27:59 +0900

    under develop

--- depth_viewer.py
+++ depth_viewer.py
@@ -1,9 +1,23 @@
-import argparse
+from pathlib import Path
 
-parser = argparse.ArgumentParser(description="depth npy file viewer")
-parser.add_argument("captured_dir", help="captured directory by capture.py")
-group = parser.add_argument_group("colormap")
-group.add_argument("--gray", help="gray colormap")
-group.add_argument("--jet", help="jet colormap")
-args = parser.parse_args()
-print(args)
+def main(args):
+    captured_dir = Path(args.captured_dir)
+    leftdir = captured_dir / "left"
+    rightdir = captured_dir / "right"
+    zeddepthdir = captured_dir / "zed-depth"
+
+    left_images = sorted(leftdir.glob("*.png"))
+    for p in sorted(zeddepthdir.glob("*.npy")):
+        print(p)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="depth npy file viewer")
+    parser.add_argument("captured_dir", help="captured directory by capture.py")
+    group = parser.add_argument_group("colormap")
+    group.add_argument("--gray", help="gray colormap")
+    group.add_argument("--jet", help="jet colormap")
+    args = parser.parse_args()
+    print(args)
