c3a8fe8f3ace464fb5dafe5cf223d7af9beff2c8
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-09-05 18:57:54 +0900

    under fixing colormap

--- capture.py
+++ capture.py
@@ -23,7 +23,7 @@ def finitemin(depth: np.ndarray):
     return np.nanmin(depth[np.isfinite(depth)])
 
 
-def depth_as_colorimage(depth_raw: np.ndarray, vmin=None, vmax=None) -> np.ndarray:
+def depth_as_colorimage(depth_raw: np.ndarray, vmin=None, vmax=None, colormap=cv2.COLORMAP_INFERNO) -> np.ndarray:
     """
     apply color mapping with vmin, vmax
     """
@@ -33,7 +33,7 @@ def depth_as_colorimage(depth_raw: np.ndarray, vmin=None, vmax=None) -> np.ndarr
         vmax = finitemax(depth_raw)
     depth_raw = (depth_raw - vmin) / (vmax - vmin) * 255.0
     depth_raw = depth_raw.astype(np.uint8)  # depth_raw might have NaN, PosInf, NegInf.
-    return cv2.applyColorMap(depth_raw, cv2.COLORMAP_INFERNO)
+    return cv2.applyColorMap(depth_raw, colormap)
 
 
 def depth_as_gray(depth_raw: np.ndarray, vmin=None, vmax=None) -> np.ndarray:
