546b2a955b5f96366fff3d246dab090971d21d27
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-09-06 14:09:43 +0900

    moved some to util.py

--- depth_viewer.py
+++ depth_viewer.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 import time
-import inspect
 
 import cv2
 import numpy as np
--- util.py
+++ util.py
@@ -34,3 +34,13 @@ def depth_as_gray(depth_raw: np.ndarray, vmin=None, vmax=None) -> np.ndarray:
     depth_raw = (depth_raw - vmin) / (vmax - vmin) * 255.0
     gray = depth_raw.astype(np.uint8)  # depth_raw might have NaN, PosInf, NegInf.
     return cv2.merge((gray, gray, gray))
+
+
+def resize_image(image: np.ndarray, rate: float) -> np.ndarray:
+    H, W = image.shape[:2]
+    return cv2.resize(image, (int(W * rate), int(H * rate)))
+
+
+def as_matrix(chw_array: np.ndarray) -> np.ndarray:
+    H_, W_ = chw_array.shape[-2:]
+    return np.reshape(chw_array, (H_, W_))
