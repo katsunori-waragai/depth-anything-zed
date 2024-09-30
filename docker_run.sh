822b046f38acb63dbcfdb39e7846401dfe151cd4
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-09-30 17:32:12 +0900

    fixed docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -2,9 +2,9 @@
 xhost +
 export GIT_ROOT=$(cd $(dirname $0)/.. ; pwd)
 docker run -it --rm --net=host --runtime nvidia -e DISPLAY=$DISPLAY \
-	-v ${GIT_ROOT}/depth-anything-zed/weights:/root/Depth-Anything-for-Jetson-Orin/weights \
-	-v ${GIT_ROOT}/depth-anything-zed/data:/root/Depth-Anything-for-Jetson-Orin/data \
-	-v ${GIT_ROOT}/depth-anything-zed/test:/root/Depth-Anything-for-Jetson-Orin/test \
+	-v ${GIT_ROOT}/depth-anything-zed/weights:/root/depth-anything-zed/weights \
+	-v ${GIT_ROOT}/depth-anything-zed/data:/root/depth-anything-zed/data \
+	-v ${GIT_ROOT}/depth-anything-zed/test:/root/depth-anything-zed/test \
 	--device /dev/bus/usb \
 	--device /dev/video0:/dev/video0:mwr \
 	-v /tmp/.X11-unix/:/tmp/.X11-unix depth-anything-zed:100
