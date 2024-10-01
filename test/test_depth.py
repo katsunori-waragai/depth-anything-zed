e795ac1195dff25caaf9e5f55eda8561e9eb0df4
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-10-01 17:23:29 +0900

    use Namespace

--- test/test_depth.py
+++ test/test_depth.py
@@ -45,12 +45,5 @@ def depth_run(args):
 
 if __name__ == "__main__":
     import argparse
-    args = argparse.ArgumentParser(description="depth-anything using zed2i as usb camera")
-    args.add_argument("--frame_rate", type=int, default=15, help="Frame rate of the camera")
-    args.add_argument("--raw", action="store_true", help="Use only the raw depth map")
-    args.add_argument("--stream", action="store_true", help="Stream the results")
-    args.add_argument("--record", action="store_true", help="Record the results")
-    args.add_argument("--save", action="store_true", help="Save the results")
-    args.add_argument("--grayscale", action="store_true", help="Convert the depth map to grayscale")
-    args = args.parse_args()
+    args = argparse.Namespace(frame_rate=15, grayscale=False, raw=False, record=False, save=False, stream=False)
     depth_run(args)
