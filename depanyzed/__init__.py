25ac95b141bc0573014678df363adf6af1e8ca51
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-10-02 10:00:57 +0900

    added to __init__.py

--- depanyzed/__init__.py
+++ depanyzed/__init__.py
@@ -1,3 +1,3 @@
 from .dpt import DepthAnything, DPT_DINOv2
-from .lib_depth_engine import DepthEngine
+from .lib_depth_engine import DepthEngine, depth_as_colorimage
 from .depthcomplementor import DepthComplementor
