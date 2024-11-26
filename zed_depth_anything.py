35445a0e91cbb164d99416a183cc140fc4223170
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-11-26 11:06:43 +0900

    as disparity

--- zed_depth_anything.py
+++ zed_depth_anything.py
@@ -123,7 +123,7 @@ def main(opt):
         assert frame.dtype == depth_any.dtype
         assert frame.shape[0] == depth_any.shape[0]
         if 1:
-            depth_colored = depanyzed.depth_as_colorimage(cv_depth_data)
+            depth_colored = depanyzed.depth_as_colorimage(np.reciprocal(cv_depth_data))
         else:
             depth_colored = np.full(frame.shape, 0, dtype=np.uint8)
 
